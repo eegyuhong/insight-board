47f986f23f934c2d6dd76b98fbe3502876a63d21
ee.gyuhong <ee.gyuhong@gmail.com>
2025-06-22 14:52:03 +0900

    visit_logs 조회 시 캐시 방지

--- components/main.tsx
+++ components/main.tsx
@@ -4,7 +4,10 @@ import VisitChart from './visit-chart';
 
 export default async function Main() {
   const supabase = createClient();
-  const { data } = await supabase.from('visit_logs').select('*');
+  const { data } = await supabase
+    .from('visit_logs')
+    .select('*')
+    .abortSignal(new AbortController().signal);
 
   const formatted = data?.map((log) => ({
     ...log,
