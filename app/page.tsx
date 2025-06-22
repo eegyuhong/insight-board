9544a7731fedf7a1da56e9b9a4bc3d3083bc0b89
ee.gyuhong <ee.gyuhong@gmail.com>
2025-06-22 15:21:24 +0900

    visit_logs 조회 시 캐시 방지

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
+
 import Header from '@/components/header';
 import Main from '@/components/main';
 import Footer from '@/components/footer';
